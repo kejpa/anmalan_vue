7f9a1d1eb76a62aab994001ad143afb614673f01
Kjell <kjell@kejpa.com>
2025-07-23 23:13:43 +0300

    Inloggning fungerar

--- src/components/LoginScreen.vue
+++ src/components/LoginScreen.vue
@@ -1,11 +1,27 @@
-<script setup></script>
+<script setup>
+import APIServices from '@/services/APIServices.js'
+import { storeAccessToken } from '@/stores/accessTokenStorage.js'
+
+function LoggaIn() {
+  APIServices.post('login', {
+    user: document.querySelector('input[name="user"]').value,
+    password: document.querySelector('input[name="password"]').value,
+  })
+    .then((data) => {
+      storeAccessToken(data.jwt)
+    })
+    .catch((error) => {
+      console.error('Fel vid inloggning:', error)
+    })
+}
+</script>
 
 <template>
   <main>
     <h1>Logga in</h1>
     <label>Användarnamn: <input type="text" placeholder="epost" name="user" /></label>
     <label>Lösenord: <input type="password" name="password" /></label>
-    <button>Logga in</button>
+    <button @click="LoggaIn()">Logga in</button>
   </main>
 </template>
 
@@ -20,5 +36,14 @@ label {
   display: block;
   width: 15em;
   margin-bottom: 1rem;
+}
+
+button {
+  padding: 0.5rem 1rem;
+  font-size: 1rem;
+}
+
+input {
+  font-size: 1rem;
 }
 </style>
--- vite.config.js
+++ vite.config.js
@@ -15,4 +15,15 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+//    port: 5174,
+    proxy: {
+      '/api': {
+        target: 'http://anmalan.localhost/',
+        changeOrigin: true,
+      },
+    },
+    cors: false,
+  },
+
 })
