7ef5c7dbd7cec3a8a28e494f9c70e15dbca44872
Kjell <kjell@kejpa.com>
2025-12-21 13:24:26 +0200

    Gjort om listan med anmälningar och lagt till funktioner för att hämta anmälningar via API

--- src/components/AddEntries.vue
+++ src/components/AddEntries.vue
@@ -2,6 +2,10 @@
 import {ref, watch} from "vue";
 import useCompetitionStore from "@/stores/competitionStore.js";
 import useSwimmerStore from "@/stores/swimmerStore.js";
+import add from '@/assets/images/add.png'
+import remove from '@/assets/images/delete.png'
+import notAllowed from '@/assets/images/notAllowed.png'
+import useEntriesStore from "@/stores/entriesStore.js";
 
 const props = defineProps(['competitionId', 'swimmer'])
 const emit = defineEmits(['close'])
@@ -9,9 +13,9 @@ const emit = defineEmits(['close'])
 const competitionStore = useCompetitionStore();
 const competition = ref({})
 const swimmerStore = useSwimmerStore();
-const swimmerResults=ref([]);
-
-const entries = ref([])
+const swimmerEvents = ref([]);
+const entriesStore = useEntriesStore();
+const entries =ref([])
 
 watch(() => props.competitionId, async (newId) => {
         if (!newId) return
@@ -30,45 +34,122 @@ watch(() => props.swimmer, async (newSwimmer) => {
         if (!newSwimmer) return
 
         try {
+            entries.value=entriesStore.getEntries(newSwimmer.id)
             const data = await swimmerStore.getResults(props.competitionId, newSwimmer.id)
-            for(let item of competition.value.events)  {
-                item.tider=data.find(itm => itm.eventid===item.eventid).tider
-                item.guldtid= data.find(itm => itm.eventid===item.eventid).guldTid.swimtime ?? null
+            for (let event of competition.value.events) {
+                let time = data.find(itm => itm.eventid === event.eventid)
+                if (time) {
+                    const se = {event}
+                    se.hasSwimmerEntry = false;
+                    se.tider = time.tider
+                    if (time.guldTid) {
+                        se.guldtid = time.guldTid.swimtime
+                    }
+                    swimmerEvents.value.push(se)
+                }
             }
         } catch (err) {
             console.error("Kunde inte hämta resultat för simmare:", err)
-            swimmerResults.value = null
+            swimmerEvents.value = null
         }
-        console.log(swimmerResults.value)
     },
     {immediate: true} // Kör direkt vid mount
 )
 
 function closeModal() {
-    for(let item of competition.value.events)  {
+    for (let item of competition.value.events) {
         delete (item.tider)
-        delete(item.guldtid)
+        delete (item.guldtid)
     }
     emit('close')
 }
+
+function addEntry(eventid, entryInfo) {
+    const entry = {}
+    entry.competitionId=props.competitionId
+    entry.swimmerid = props.swimmer.id
+    entry.eventid = eventid
+    entry.info = entryInfo
+
+    // Lägg till entry om eventid inte finns eller bassängen är olika
+    entries.value.push(entry)
+    swimmerEvents.value.find(itm => itm.event.eventid === eventid).hasSwimmerEntry = true;
+
+    // Sortera entries efter grennummer
+    entries.value.sort((a,b)=>{
+        return competition.value.events.find(e => e.eventid===a.eventid).number-competition.value.events.find(e => e.eventid===b.eventid).number
+    })
+}
+
+function removeEntry(entry){
+    entries.value=entries.value.filter(itm => itm !== entry);
+    swimmerEvents.value.find(itm => itm.event.eventid === entry.eventid).hasSwimmerEntry = false;
+}
+
 </script>
 
 <template>
     <div class="modal">
-
-        <div id="entries">
+        <main>
             <div id="close" @click="closeModal()">×</div>
             <h1>Anmälningar för {{ props.swimmer?.firstname ?? 'Lasse-gurra' }}
                 {{ props.swimmer?.lastname ?? 'Aktersnurra' }}
                 ({{ props.swimmer?.license ? props.swimmer.license : 'Ingen licens' }})</h1>
-            <ul v-for="event in competition.events" :key="event.id">
-                <li>{{ event.name }}</li>
-                <li>{{ event.utmanare ? "Utmanare" : "Vanlig gren" }}</li>
-                <li>{{ event?.tider  ??'' }}</li>
-                <li>{{ event?.guldtid  ??'' }}</li>
-             </ul>
-            {{swimmerResults}}
-        </div>
+            <ul v-for="event in swimmerEvents.filter(e => e.hasSwimmerEntry===false)"
+                :key="event.event.eventid">
+                <li>{{ event.event.number }}</li>
+                <li>{{ `${event.event.name} ${event.event.utmanare ? " (Utmanare)" : ""}` }}</li>
+                <li v-if="event.tider && event.event.utmanare"
+                    :title="event.tider.SCM?.swimtime<event.guldtid ? 'Uppnådd tid bättre än guldtiden' :''">
+                    <span v-if="event.tider.SCM?.swimtime" class="entrytimes">
+                    <span v-if="event.tider.SCM?.swimtime>event.guldtid" class="enterEvent"><img
+                        :src="add"
+                        @click="addEntry(event.event.eventid,event.tider.SCM, $event)"></span>
+                    <span v-else class="enterEvent"><img :src="notAllowed"></span>
+                        {{ event.tider.SCM?.swimtime ? `${event.tider.SCM?.swimtime} (25m)` : '' }}
+                    </span>
+                </li>
+                <li v-if="event.tider && !event.event.utmanare">
+                    <span v-if="event.tider.SCM?.swimtime" class="entrytimes">
+                        <span class="enterEvent">
+                            <img :src="add"
+                                 @click="addEntry(event.event.eventid,event.tider.SCM, $event)">
+                        </span>
+                        {{ event.tider.SCM?.swimtime ? `${event.tider.SCM?.swimtime} (25m)` : '' }}
+                    </span>
+                    <span v-if="event.tider.LCM?.swimtime" class="entrytimes">
+                        <span class="enterEvent">
+                            <img :src="add"
+                                 @click="addEntry(event.event.eventid,event.tider.LCM, $event)">
+                        </span>
+                        {{ event.tider.LCM?.swimtime ? `${event.tider.LCM?.swimtime} (50m)` : '' }}
+                    </span>
+                    <span v-if="event.tider.length===0" class="entrytimes">Inga tider angiven</span>
+                </li>
+                <li v-else>&nbsp;</li>
+            </ul>
+            <hr>
+            <h2>Anmälda grenar</h2>
+            <ul v-for="entry in entries" :key="entry">
+                <li><span class="enterEvent"><img :src="remove" @click="removeEntry(entry)"></span>
+                    {{
+                        competition.events.find(e => e.eventid == entry.eventid)?.number ?? entry.eventid
+                    }}
+                </li>
+                <li>{{
+                        competition.events.find(e => e.eventid === entry.eventid)?.name
+                    }}
+                </li>
+                <li>{{
+                        competition.events.find(e => e.eventid === entry.eventid)?.utmanare ? " (Utmanare)" : ""
+                    }}
+                </li>
+                <li>{{
+                        `${entry.info.swimtime} ${entry.info.course === 'SCM' ? '(25)' : '(50m)'}`
+                    }}
+                </li>
+            </ul>
+        </main>
     </div>
 </template>
 
@@ -97,7 +178,7 @@ function closeModal() {
     cursor: pointer;
 }
 
-#entries {
+main {
     padding: 5px;
     height: 80%;
     width: 80%;
@@ -106,16 +187,27 @@ function closeModal() {
     border: 3px solid black;
     background-color: white;
 }
+
 ul {
     display: flex;
     list-style-type: none;
     flex-direction: row;
     background-color: lightgray;
 }
+
 ul:nth-child(odd) {
     background-color: white;
 }
+
 li {
     padding: 5px;
+}
+
+span.entrytimes {
+    display: block;
+}
+
+.enterEvent img {
+    height: 1em;
 }
 </style>
---
+++ src/stores/entriesStore.js
@@ -0,0 +1,100 @@
+import {defineStore} from 'pinia'
+import {ref} from 'vue'
+import APIServices from "@/services/APIServices.js";
+
+const useEntriesStore = defineStore('entriesStore', () => {
+    const allEntries = ref([])
+    const isLoading = ref(false)
+    const hasLoaded = ref(false)
+    let loadPromise = null // används för att dela samma promise
+
+    /*  const entry = ref({
+          id: '',
+          swimmerId: '',
+          competitionId: '',
+          eventId: '',
+          info: [{
+              time: '',
+              city: '',
+              date: '',
+              course: 'SCM',
+          }
+          ],
+      })
+  */
+
+    async function getEntries(swimmerId) {
+        // Se till att all data finns innan vi letar
+        if (!hasLoaded.value) {
+            await getAll()
+        }
+
+        // Returnera posterna
+        return allEntries.value.filter((swimmer) => swimmer.id === swimmerId);
+    }
+
+    async function getAll(competitionId) {
+        if (hasLoaded.value) return allEntries.value
+        if (isLoading.value && loadPromise) return loadPromise
+
+        isLoading.value = true
+        loadPromise = (async () => {
+            try {
+                const data = await APIServices.get(`getEntries?competitionId=${competitionId}`)
+                allEntries.value = data
+                hasLoaded.value = true
+            } catch (error) {
+                console.error('Error fetching entries:', error)
+                allEntries.value = []
+            } finally {
+                isLoading.value = false
+            }
+            return allEntries.value
+        })()
+
+        return loadPromise
+    }
+
+    function saveEntry(entry) {
+        return new Promise((resolve, reject) => {
+            if (entry.id) {
+                APIServices.put('saveEntry', entry)
+                    .then(() => {
+                        resolve(true)
+                    })
+                    .catch((error) => {
+                        console.error('Error updating entry:', error)
+                        reject(error)
+                    })
+            } else {
+                APIServices.post('saveEntry', entry)
+                    .then((data) => {
+                        entry.id = data.id
+                        allEntries.value = allEntries.value.push(entry)
+                        resolve(true)
+                    })
+                    .catch((error) => {
+                        console.error('Error creating entry:', error)
+                        reject(error)
+                    })
+            }
+        })
+    }
+
+    function removeEntry(entryId) {
+        return new Promise((resolve, reject) => {
+            APIServices.post('deleteEntry', entryId)
+                .then(() => {
+                    allEntries.value = allEntries.value.filter(e => e.id !== entryId)
+                    resolve(true)
+                })
+                .catch((error) => {
+                    console.error('Error removing entry:', error)
+                    reject(error)
+                })
+        })
+    }
+
+    return {saveEntry, getAll, getEntries, removeEntry}
+})
+export default useEntriesStore
